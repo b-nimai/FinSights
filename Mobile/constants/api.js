f85878993d73e6ef7b605fa321fea9bb7845f0b9
b-nimai <nimaibarman4978@gmail.com>
2025-06-22 12:15:37 +0530

    V.1.0.0

--- Mobile/constants/api.js
+++ Mobile/constants/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://finsights-p6kc.onrender.com/api";
-export const API_URL = "http://10.0.2.2:3000/api";
+export const API_URL = "https://finsights-p6kc.onrender.com/api";
+// export const API_URL = "http://10.0.2.2:3000/api";
